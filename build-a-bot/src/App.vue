19912ae9c2f15c9f6db02c86075438860888310c
Lee <leerobertcant@gmail.com>
2019-08-01 21:59:35 +0100

    Created home page

---
+++ build-a-bot/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" />
+            Build-a-bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: "app",
+  components: {
+    HomePage
+  }
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
